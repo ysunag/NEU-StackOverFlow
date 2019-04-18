538b44cad96f55dc55deaed04bfd3594ea64046f
flashyang <flashyangli@gmail.com>
2019-04-18 15:35:14 -0700

    add unique id for facebook user

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -2,6 +2,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require("bcrypt-nodejs");
 var FacebookStrategy = require('passport-facebook').Strategy;
+var uniqid = require('uniqid');
+
 
 module.exports = function (app) {
 
@@ -66,6 +68,7 @@ module.exports = function (app) {
         } else {
           var names = profile.displayName.split(" ");
           var newFacebookUser = {
+            username: 'fb' + uniqid(),
             lastName: names[1],
             firstName: names[0],
             email: profile.emails ? profile.emails[0].value:"",
